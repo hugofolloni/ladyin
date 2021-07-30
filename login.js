935d27c7b6f09e1ad8aad373daa5b1d4cd685eb4
Hugo Folloni <hugofollogua07@gmail.com>
2021-07-30 18:50:40 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+var username = document.getElementById('username');
+var button = document.getElementById('button');
+
+button.addEventListener('click', saveUsername);
+
+function saveUsername(){
+    var usernameLogin = username.value;
+    localStorage.setItem('username', usernameLogin)
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+var nameLabel = document.getElementById('nome');
+var username = localStorage.getItem('username');
+console.log(username)
+
+nameLabel.textContent = username
+
+
