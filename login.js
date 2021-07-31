7e822a5655d3c6fa44337703bc219ff2a8590b7b
Hugo Folloni <hugofollogua07@gmail.com>
2021-07-30 22:25:57 -0300

    Update searchInput

--- login.js
+++ login.js
@@ -7,3 +7,4 @@ function saveUsername(){
     var usernameLogin = username.value;
     localStorage.setItem('username', usernameLogin)
 }
+
--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var nameLabel = document.getElementById('nome');
 var username = localStorage.getItem('username');
-console.log(username)
+var inputSearch = document.getElementById('inputSearch')
 
 nameLabel.textContent = username
+
+inputSearch.addEventListener('keyup', searchOnSite);
+
+function searchOnSite(){
+    if (event.key == "Enter"){
+        window.open('error.html', '_blank');
+    }
+}
 
 
